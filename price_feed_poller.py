d6176f4f9601a017d5f5ba2c70792d881f94cadc
Guillermo Díaz <guillermo@otherplane.com>
2021-11-04 10:38:09 +0100

    chore: improve logs readability

--- price_feed_poller.py
+++ price_feed_poller.py
@@ -29,15 +29,16 @@ def handle_requestUpdate(
 
       if balance == 0:
           raise Exception("Account does not have any funds")
-      print(f"Balance: {balance} wei")
+      print(f"Balance: {round(balance / 10 ** 18, 5)} ETH")
 
       if gas_price is None:
         print(f"Estimating gas price from last blocks...")
         gas_price = w3.eth.generateGasPrice()
-      print(f"Gas price: {gas_price}")
+      print("Gas price:", "{:,}".format(gas_price))
+      print("Gas:", "{:,}".format(gas))
 
       reward = wrbcontract.functions.estimateReward(gas_price).call()
-      print(f"Reward: {reward}")
+      print(f"Reward: {round(reward / 10 ** 18, 5)} ETH")
 
       dr_id = pricefeedcontract.functions.requestUpdate().transact({
         "from": account_addr,
@@ -50,6 +51,7 @@ def handle_requestUpdate(
       # Get receipt of the transaction
       print(f"Requesting update on {pricefeedcontract.address} (tx: {dr_id.hex()})...")
       receipt = w3.eth.waitForTransactionReceipt(dr_id, tx_waiting_timeout_secs, tx_polling_latency_secs)
+      print("Total cost:", round((balance - w3.eth.getBalance(account_addr)) / 10 ** 18, 5), "ETH")
 
     except exceptions.TimeExhausted:
       print(f"Transaction for requesting update on {pricefeedcontract.address} is taking too long. Retrying in next iteration.")
@@ -90,12 +92,15 @@ def handle_completeUpdate(
       balance = w3.eth.getBalance(account_addr)
       if balance == 0:
         raise Exception("Account does not have any funds")
-      print(f"Balance: {balance} wei")
+      
+      print(f"Balance: {round(balance / 10 ** 18, 5)} ETH")
 
       if gas_price is None:
         print(f"Estimating gas price from last blocks...")
         gas_price = w3.eth.generateGasPrice()
-      print(f"Gas price: {gas_price}")
+      
+      print("Gas price:", "{:,}".format(gas_price))
+      print("Gas:", "{:,}".format(gas))
 
       read_id = pricefeedcontract.functions.completeUpdate().transact({
         "from": account_addr,
@@ -107,6 +112,7 @@ def handle_completeUpdate(
       # Get receipt of the transaction
       print(f"Completing update on {pricefeedcontract.address} (tx: {read_id.hex()})...")
       receipt = w3.eth.waitForTransactionReceipt(read_id, tx_waiting_timeout_secs, tx_polling_latency_secs)
+      print("Total cost:", round((balance - w3.eth.getBalance(account_addr)) / 10 ** 18, 5), "ETH")
 
     except exceptions.TimeExhausted:
       print(f"Transaction for completing update on {pricefeedcontract.address} is taking too long. Retrying in next iteration.")
