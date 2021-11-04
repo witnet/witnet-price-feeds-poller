05e7761a412f0c764d73f9336cb9a0bb0c81c97e
Guillermo Díaz <guillermo@otherplane.com>
2021-11-04 13:31:24 +0100

    chore: extract network name from full path of csv file

--- price_feed_poller.py
+++ price_feed_poller.py
@@ -140,7 +140,7 @@ def log_master_balance(csv_filename, addr, balance, txhash):
   if csv_filename is not None:
     try:
       with open(csv_filename, "a", encoding="utf-8") as csv_file:
-        row = f"\"{os.path.splitext(csv_filename)[0]}\";\"{addr}\";\"{int(time.time())}\";\"{balance}\";\"{txhash}\""
+        row = f"\"{os.path.splitext(os.path.basename(csv_filename))[0]}\";\"{addr}\";\"{int(time.time())}\";\"{balance}\";\"{txhash}\""
         csv_file.write(row + '\n')
     except:
       return
